1dc72d2c25ea7c3a61d0ae40a7c2425cf009391b
aaleeks2 <aleksanderlapinski@wp.pl>
2023-12-03 00:02:33 +0100

    add part 1 for 9-2022

--- 2022/D9/RopeBridge.js
+++ 2022/D9/RopeBridge.js
@@ -3,8 +3,11 @@ const fs = require("fs");
 const positions = new Set();
 
 const X_SURFACE = ["L", "R"];
-const INCREMENTS = ["U", "R"];
+const RIGHT = 'R';
+const UP = 'U';
 
+
+// Part 1
 fs.readFile("rope_bridge.txt", "utf-8", (err, data) => {
     if (err) {
       console.error(err);
@@ -22,25 +25,55 @@ fs.readFile("rope_bridge.txt", "utf-8", (err, data) => {
     };
 
     const splittedLines = data.split("\n");
-
+    positions.add('[0-0]')
     splittedLines.forEach((line) => {
         const splittedLine = line.split(" ");
         const direction = splittedLine[0];
-        const distance = Number(splittedLine[1]);
+        const distance = parseInt(splittedLine[1]);
         const newCoordinate = X_SURFACE.includes(direction)
-            ? { x: INCREMENTS.includes(direction) ? head.x + distance : head.x - distance, y: head.y}
-            : { x: head.x, y: INCREMENTS.includes(direction) ? head.y + distance : head.y - distance};
+            ? { x: direction === RIGHT ? head.x + distance : head.x - distance, y: head.y}
+            : { x: head.x, y: direction === UP ? head.y + distance : head.y - distance};
         head = newCoordinate;
-
-        if(!isAdjecent(head, tail)) {
+        
+        if(!tailIsAdjecentToHead(head, tail)) {
             fixTailPosition(head, tail);
         }
-
     });
+
+    console.log(positions.size);
 });
 
 function fixTailPosition(head, tail) {
+    if(tailIsInDifferentRow(head, tail)) {
+        const x_diff = head.x - tail.x;
+        const y_diff = head.y - tail.y;
+
+        if(Math.abs(x_diff) == 1) {
+            changePosition(
+                tail, 
+                tail.x + x_diff,
+                tail.y + (head.y > tail.y ? 1 : -1)
+            );
+        } else if(Math.abs(y_diff) == 1) {
+            changePosition(
+                tail,
+                tail.x + (head.x > tail.x ? 1 : -1),
+                tail.y + y_diff
+            );
+        }
+    }
+
+    const axis = head.x != tail.x ? 'x' : 'y';
+    const distance = axis === 'x' ? head.x - tail.x : head.y - tail.y;
+    const shouldGoForward = distance > 0;
     
+    for(let i = 0; i < Math.abs(distance) - 1; ++i) {
+        if(axis === 'x') {
+            changePosition(tail, tail.x + (shouldGoForward ? 1 : -1), tail.y);
+        } else {
+            changePosition(tail, tail.x, tail.y + (shouldGoForward ? 1 : -1));
+        }
+    }
 }
 
 function tailIsInDifferentRow(head, tail) {
@@ -48,9 +81,13 @@ function tailIsInDifferentRow(head, tail) {
 }
 
 function tailIsAdjecentToHead(head, tail) {
-    return Math.abs(head.x - tail.x) <= 1 && Math.abs(head.y - tail.y) <= 1
+    return Math.abs(head.x - tail.x) <= 1 && Math.abs(head.y - tail.y) <= 1;
 }
 
-function savePosition(tail) {
-    positions.add(String(tail.x) + ":" + String(tail.y));
+function changePosition(node, x, y) {
+    node.x = x;
+    node.y = y;
+    positions.add(`[${String(node.x)}-${String(node.y)}]`);
 }
+
+// Part 2
