eeb3080b1aff532f166abca2e3e536dd7cc3cc6e
aaleeks2 <aleksanderlapinski@wp.pl>
2023-12-02 18:28:07 +0100

    format code

--- 2023/D2/d2.js
+++ 2023/D2/d2.js
@@ -1,10 +1,29 @@
 const fs = require("fs");
+const StringFormatter = require("../../2022/Utility/StringFormatter.js");
+
+const TEMPLATE = "{0}: {1}";
+const NEW_LINE = "\n";
+const REGEX_NUMBER = /[0-9]+/g;
+const SPACE = / /g;
+const COLON_SPACE = /: /g;
+const R = 'r'
+const G = 'g'
+const B = 'b'
+
 fs.readFile("input.txt", "utf-8", (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log(data.split("\n").reduce(callbackFn2, 0));
+    console.log(StringFormatter.formatWith1ArgForPart1(
+        TEMPLATE, 
+        data.split(NEW_LINE).reduce(callbackFn, 0)
+    ));
+
+    console.log(StringFormatter.formatWith1ArgForPart2(
+        TEMPLATE, 
+        data.split(NEW_LINE).reduce(callbackFn2, 0)
+    ));
 });
 
 // Part 1
@@ -14,20 +33,20 @@ callbackFn = (accumulator, line) => {
     let bMax = -1;
 
     const gameSplit = line.split(": ");
-    const gameId = Number(gameSplit[0].match(/[0-9]+/g));
+    const gameId = parseInt(gameSplit[0].match(REGEX_NUMBER));
  
     const cubesSplit = gameSplit[1].split(" ");
    
     for(let i = 1; i < cubesSplit.length; i = i + 2) {
-        let val = Number(cubesSplit[i-1]);
+        let val = parseInt(cubesSplit[i-1]);
         let color = cubesSplit[i].charAt(0);
 
-        if(color == 'r') {
-            rMax = val >= rMax ? val : rMax;
-        } else if(color == 'g') {
-            gMax = val >= gMax ? val : gMax;
-        } else {
-            bMax = val >= bMax ? val : bMax;
+        if(color == R && val >= rMax) {
+            rMax = val;
+        } else if(color == G && val >= gMax) {
+            gMax = val;
+        } else if(color == B && val >= bMax){
+            bMax = val;
         }
 
         if(rMax > 12 || gMax > 13 || bMax > 14) {
@@ -44,18 +63,18 @@ callbackFn2 = (accumulator, line) => {
     let gMax = -1;
     let bMax = -1;
 
-    const cubesSplit = line.split(": ")[1].split(" ");
+    const cubesSplit = line.split(COLON_SPACE)[1].split(SPACE);
    
     for(let i = 1; i < cubesSplit.length; i = i + 2) {
-        let val = Number(cubesSplit[i-1]);
+        let val = parseInt(cubesSplit[i-1]);
         let color = cubesSplit[i].charAt(0);
 
-        if(color == 'r') {
-            rMax = val >= rMax ? val : rMax;
-        } else if(color == 'g') {
-            gMax = val >= gMax ? val : gMax;
-        } else {
-            bMax = val >= bMax ? val : bMax;
+        if(color == R && val >= rMax) {
+            rMax = val;
+        } else if(color == G && val >= gMax) {
+            gMax = val;
+        } else if(color == B && val >= bMax){
+            bMax = val;
         }
     }
     return accumulator + rMax * gMax * bMax;
