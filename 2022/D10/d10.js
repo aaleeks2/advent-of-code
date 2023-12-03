de4300f05e058c3a9dbc4b9651e77d1f0fb72c06
aaleeks2 <aleksanderlapinski@wp.pl>
2023-12-03 22:41:42 +0100

    add part 2

--- 2022/D10/d10.js
+++ 2022/D10/d10.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const IS_TEST = true;
+const IS_TEST = false;
 const INPUT = 'input.txt';
 const INPUT_TEST = 'test_input.txt';
 const ENCODING_UTF_8 = 'utf-8';
@@ -7,7 +7,8 @@ const NEW_LINE = '\n';
 const SPACE = ' ';
 const NOOP = 'noop';
 const THRESHOLDS = [20, 60, 100, 140, 180, 220];
-let cycles = 0;
+const THRESHOLDS_2 = [40, 80, 120, 160, 200];
+let cycles = 1;
 let sum = 0;
 
 fs.readFile(IS_TEST ? INPUT_TEST : INPUT, ENCODING_UTF_8, (err, data) => {
@@ -16,8 +17,8 @@ fs.readFile(IS_TEST ? INPUT_TEST : INPUT, ENCODING_UTF_8, (err, data) => {
       return;
     }
    
-    // data.split(NEW_LINE).reduce(callback, 1);
-    // console.log(`Sum: ${sum}`);
+    data.split(NEW_LINE).reduce(callback, 1);
+    console.log(`Sum: ${sum}`);
     render(data);
 });
 
@@ -37,13 +38,16 @@ callback = (accumulator, line) => {
 // Part 2
 render = (data) => {
     let result = ['', '', '', '', '', ''];
-    // let signalCounter = 0;
-    let accumulator = 0;
+    let signalCounter = 0;
+    let accumulator = 1;
+    let currentRow = 0;
+
     for(let line of data.split(NEW_LINE)) {
         const splittedLine = line.trim().split(SPACE);
-        for(let i = 0; i < (splittedLine[0] == NOOP ? 1 : 2); ++i) {
-            cycles++;
-            console.log(`X: ${accumulator}`);
+        const isNoop = splittedLine[0] == NOOP;
+        
+        for(let i = 0; i < (isNoop ? 1 : 2); ++i) {
+            console.log(`## Cycle ${cycles}: START ##`);
             let spriteStart = 0;
             let spriteEnd = 0;
             if(accumulator < 1) {
@@ -52,30 +56,29 @@ render = (data) => {
                 spriteStart = accumulator - 1;
                 spriteEnd = accumulator + 1;
             }
-            console.log(`Sprite position: [${spriteStart}:${spriteEnd}]`);
 
-            let currentRow;
-            if(cycles < 40) {
-                currentRow = 0;
-            } else if(cycles < 80) {
-                currentRow = 1;
-            } else if(cycles < 120) {
-                currentRow = 2;
-            } else if(cycles < 160) {
-                currentRow = 3;
-            } else if(cycles < 200) {
-                currentRow = 4;
-            } else {
-                currentRow = 5;
-            }
+            console.log(`Current CRT position\t${signalCounter}`);
+            console.log(`Sprite position:\t[${spriteStart}:${spriteEnd} | X: ${accumulator}]`)
 
-            if(accumulator >= spriteStart && accumulator <= spriteEnd) {
+            if(signalCounter >= spriteStart && signalCounter <= spriteEnd) {
                 result[currentRow] = result[currentRow].concat('*');
             } else {
                 result[currentRow] = result[currentRow].concat('.');
             }
+            console.log(`Row updated:\t${result[currentRow]}`);
+            if(THRESHOLDS_2.includes(cycles)) {
+                currentRow++;
+                signalCounter = 0;
+            } else {
+                signalCounter++;
+            }
+            console.log(`## Cycle ${cycles}: END ##\n`);
+            cycles++;
         }
-        accumulator += (splittedLine[0] != NOOP ? parseInt(splittedLine[1]) : 0);
+        console.log(`After cycle ${cycles - 1} added ${!isNoop ? parseInt(splittedLine[1]) : 0}\n`);
+        accumulator += (!isNoop ? parseInt(splittedLine[1]) : 0);
     }
+    
+    console.log('###########\tTHE RESULT\t###########\n');
     console.log(result.join('\n'));
 };
