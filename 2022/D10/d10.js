262f872cab38953c28b55008b744961f81fe0c3f
Aleks <aleksanderlapinski@wp.pl>
2023-12-03 17:39:13 +0100

    add y-2022-day-10 pt2 draft

--- 2022/D10/d10.js
+++ 2022/D10/d10.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const IS_TEST = false;
+const IS_TEST = true;
 const INPUT = 'input.txt';
 const INPUT_TEST = 'test_input.txt';
 const ENCODING_UTF_8 = 'utf-8';
@@ -16,8 +16,9 @@ fs.readFile(IS_TEST ? INPUT_TEST : INPUT, ENCODING_UTF_8, (err, data) => {
       return;
     }
    
-    data.split(NEW_LINE).reduce(callback, 1);
-    console.log(`Sum: ${sum}`);
+    // data.split(NEW_LINE).reduce(callback, 1);
+    // console.log(`Sum: ${sum}`);
+    render(data);
 });
 
 // Part 1
@@ -29,5 +30,52 @@ callback = (accumulator, line) => {
             sum += accumulator * cycles;
         }
     }
+    console.log(`X: ${accumulator}`);
     return accumulator + (splittedLine[0] != NOOP ? parseInt(splittedLine[1]) : 0);
+};
+
+// Part 2
+render = (data) => {
+    let result = ['', '', '', '', '', ''];
+    // let signalCounter = 0;
+    let accumulator = 0;
+    for(let line of data.split(NEW_LINE)) {
+        const splittedLine = line.trim().split(SPACE);
+        for(let i = 0; i < (splittedLine[0] == NOOP ? 1 : 2); ++i) {
+            cycles++;
+            console.log(`X: ${accumulator}`);
+            let spriteStart = 0;
+            let spriteEnd = 0;
+            if(accumulator < 1) {
+                spriteEnd = spriteStart + 2;
+            } else {
+                spriteStart = accumulator - 1;
+                spriteEnd = accumulator + 1;
+            }
+            console.log(`Sprite position: [${spriteStart}:${spriteEnd}]`);
+
+            let currentRow;
+            if(cycles < 40) {
+                currentRow = 0;
+            } else if(cycles < 80) {
+                currentRow = 1;
+            } else if(cycles < 120) {
+                currentRow = 2;
+            } else if(cycles < 160) {
+                currentRow = 3;
+            } else if(cycles < 200) {
+                currentRow = 4;
+            } else {
+                currentRow = 5;
+            }
+
+            if(accumulator >= spriteStart && accumulator <= spriteEnd) {
+                result[currentRow] = result[currentRow].concat('*');
+            } else {
+                result[currentRow] = result[currentRow].concat('.');
+            }
+        }
+        accumulator += (splittedLine[0] != NOOP ? parseInt(splittedLine[1]) : 0);
+    }
+    console.log(result.join('\n'));
 };
