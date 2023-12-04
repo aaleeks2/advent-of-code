6185936b081c350e9dd450edcc5bf0fcf540cb0b
aaleeks2 <aleksanderlapinski@wp.pl>
2023-12-04 22:56:02 +0100

    adjust d4/2023

--- 2023/D4/d4.js
+++ 2023/D4/d4.js
@@ -7,7 +7,7 @@ const PIPE_SYMBOL = ' | ';
 const SPACE = ' ';
 const EMPTY_STR = '';
 const COLON_SPACE = ': ';
-const NUMBER_REGEX = /[0-9]+/g;
+const NUMBER_REGEX = /\d+/g;
 const WHITESPACE_REGEX = /\s+|\n/g;
 
 fs.readFile(IS_TEST ? TEST_INPUT : INPUT, "utf-8", (err, data) => {
@@ -20,12 +20,12 @@ fs.readFile(IS_TEST ? TEST_INPUT : INPUT, "utf-8", (err, data) => {
 });
 
 // Part 1
-part1 = (accumulator, line) => {
+const part1 = (accumulator, line) => {
     const winning = countWinning(line);
     return accumulator + (winning > 0 ? 2**(winning - 1) : 0);
 }
 
-countWinning = (line) => {
+const countWinning = (line) => {
     const splittedLine = line.split(PIPE_SYMBOL);
     const pickedNumbers = splittedLine[1]
         .split(WHITESPACE_REGEX)
@@ -39,13 +39,13 @@ countWinning = (line) => {
     return res.length;
 }
 // Part 2
-part2 = (data) => {
+const part2 = (data) => {
     const result = [];
     data.split(NEW_LINE).forEach(line => processGame(line, result));
     console.log(`Part 2 result: ${result.length}`);
 }
 
-processGame = (line, resultArray) => {
+const processGame = (line, resultArray) => {
     const game = parseInt(getNumberGame(line));
     resultArray.push(game);
 
@@ -57,8 +57,7 @@ processGame = (line, resultArray) => {
             resultArray.push(game+i);
             temp--;
         } while(temp >= 0)
-        temp = copies;
     }
 }
 
-getNumberGame = (line) => line.split(COLON_SPACE)[0].match(NUMBER_REGEX);
+const getNumberGame = (line) => line.split(COLON_SPACE)[0].match(NUMBER_REGEX);
